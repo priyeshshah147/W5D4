cdcbf4ed1337ba16ab99dc2b8205cd1e31efb73b
Ali Ahmed <42854126+aahmed019@users.noreply.github.com>
2021-05-20 14:10:23 -0400

    added methods for all models

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -1,2 +1,19 @@
 class Course < ApplicationRecord
+
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+    has_many :enrolled_students,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment
+
+    has_many :instructor,
+        # primary_key: :id,
+        # foreign_key: :instructor_id,
+        # class_name: :User
+        through: :enrollments,
+        source: :user
 end
